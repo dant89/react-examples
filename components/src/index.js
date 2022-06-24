6957f253ebe8f34f455b45abc9f8c907f5982ffb
Dan Thorpe <dant@linx.net>
2022-06-24 16:39:20 +0100

    Add styles

--- components/src/index.js
+++ components/src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from "react-dom";
 import { faker } from "@faker-js/faker";
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
+import './style/App.css'
 
 const App = () => {
   return (
     <div className="ui container comments">
+        <ApprovalCard>
+            <div>
+                <h4>Warning!</h4>
+            </div>
+            Are you sure you want to do this?
+        </ApprovalCard>
+        
         <ApprovalCard>
             <CommentDetail
                 author={faker.name.firstName()}
@@ -15,18 +23,22 @@ const App = () => {
                 avatar={faker.image.avatar()}
             />
         </ApprovalCard>
-        <CommentDetail
-            author={faker.name.firstName()}
-            timeAgo="Today at 2:00m"
-            comment={faker.random.words()}
-            avatar={faker.image.avatar()}
-        />
-        <CommentDetail
-            author={faker.name.firstName()}
-            timeAgo="Yesterday at 7:00am"
-            comment={faker.random.words()}
-            avatar={faker.image.avatar()}
-        />
+        <ApprovalCard>
+            <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="Today at 2:00m"
+                comment={faker.random.words()}
+                avatar={faker.image.avatar()}
+            />
+        </ApprovalCard>
+        <ApprovalCard>
+            <CommentDetail
+                author={faker.name.firstName()}
+                timeAgo="Yesterday at 7:00am"
+                comment={faker.random.words()}
+                avatar={faker.image.avatar()}
+            />
+        </ApprovalCard>
     </div>
   );
 };
